bd775d7dd59dbc510c6e5c58ba837a8f500a6bd9
npolovnikov <npolovnikov@techinfocom.com>
2016-03-03 11:23:08 +0500

    LVL 13

---
+++ src/com/javarush/test/level12/lesson04/task03/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.test.level12.lesson04.task03;
+
+/* Пять методов print с разными параметрами
+Написать пять методов print с разными параметрами.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int x){
+        System.out.print(x);
+    }
+    public static void print(Integer x){
+        System.out.print(x);
+    }
+    public static void print(String x){
+        System.out.print(x);
+    }
+    public static void print(float x){
+        System.out.print(x);
+    }
+    public static void print(char x){
+        System.out.print(x);
+    }
+
+}
